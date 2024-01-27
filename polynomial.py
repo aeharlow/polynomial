67ed234d7221e4495131e32022241347f32df6b9
Ashley Harlow <aeharlow@bu.edu>
2024-01-27 17:01:37 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -4,6 +4,9 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, x):
+        return x
 
 class Int:
     def __init__(self, i):
@@ -11,6 +14,9 @@ class Int:
     
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate(self, x):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -20,6 +26,24 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
     
+    def evaluate(self, x):
+        if isinstance(self.p2, Add):
+            a = self.p1.evaluate(x)
+            b = self.p2.p1.evaluate(x)
+            c = self.p2.p2
+            ret = Add(Int(a+b), c).evaluate(x)
+        elif isinstance(self.p2, Sub):
+            a = self.p1.evaluate(x)
+            b = self.p2.p1.evaluate(x)
+            c = self.p2.p2
+            ret = Add(Int(a+b), c).evaluate(x)
+        else:
+            val1 = self.p1.evaluate(x)
+            val2 =  self.p2.evaluate(x)
+            ret = val1 + val2
+        
+        return ret
+    
 class Sub:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -27,6 +51,24 @@ class Sub:
     
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
+    
+    def evaluate(self, x):
+        if isinstance(self.p2, Add):
+            a = self.p1.evaluate(x)
+            b = self.p2.p1.evaluate(x)
+            c = self.p2.p2
+            ret = Add(Int(a-b), c).evaluate(x)
+        elif isinstance(self.p2, Sub):
+            a = self.p1.evaluate(x)
+            b = self.p2.p1.evaluate(x)
+            c = self.p2.p2
+            ret = Sub(Int(a-b), c).evaluate(x)
+        else:
+            val1 = self.p1.evaluate(x)
+            val2 =  self.p2.evaluate(x)
+            ret = val1 - val2
+        
+        return ret
 
 class Mul:
     def __init__(self, p1, p2):
@@ -41,6 +83,24 @@ class Mul:
         if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
+    
+    def evaluate(self, x):
+        if isinstance(self.p2, Mul):
+            a = self.p1.evaluate(x)
+            b = self.p2.p1.evaluate(x)
+            c = self.p2.p2
+            ret = Mul(Int(a*b), c).evaluate(x)
+        elif isinstance(self.p2, Div):
+            a = self.p1.evaluate(x)
+            b = self.p2.p1.evaluate(x)
+            c = self.p2.p2
+            ret = Div(Int(a*b), c).evaluate(x)
+        else:
+            val1 = self.p1.evaluate(x)
+            val2 =  self.p2.evaluate(x)
+            ret = val1 * val2
+        
+        return ret
 
 class Div:
     def __init__(self, p1, p2):
@@ -56,21 +116,78 @@ class Div:
             return repr(self.p1) + " / ( " + repr(self.p2) + " )"
         return repr(self.p1) + " / " + repr(self.p2)
 
+    def evaluate(self, x):
+        if self.p2.evaluate(x) == 0 :
+            exit("cannot divide by 0")
+        else:
+            if isinstance(self.p2, Mul):
+                a = self.p1.evaluate(x)
+                b = self.p2.p1.evaluate(x)
+                c = self.p2.p2
+                ret = Mul(Int(a/b), c).evaluate(x)
+            elif isinstance(self.p2, Div):
+                a = self.p1.evaluate(x)
+                b = self.p2.p1.evaluate(x)
+                c = self.p2.p2
+                ret = Div(Int(a/b), c).evaluate(x)
+            else:
+                val1 = self.p1.evaluate(x)
+                val2 =  self.p2.evaluate(x)
+                ret = val1 / val2
+        return ret
+
+poly = Add(Int(50), Add(Int(5), Add(Int(2), Int(1))))
+print(poly)
+print(poly.evaluate(-1))
+
+poly = Add(Int(50), Mul(Int(5), Add(Int(2), Int(1))))
+print(poly)
+print(poly.evaluate(-1))
+
+poly = Sub(Int(50), Sub(Int(5), Sub(Int(2), Int(1))))
+print(poly)
+print(poly.evaluate(-1))
+
+poly = Sub(Int(50), Add(Int(5), Sub(Int(2), Int(1))))
+print(poly)
+print(poly.evaluate(-1))
+
+poly = Mul(Int(50), Mul(Int(5), Mul(Int(2), Int(1))))
+print(poly)
+print(poly.evaluate(-1))
+
+poly = Div(Int(50), Div(Int(5), Div(Int(2), Int(1))))
+print(poly)
+print(poly.evaluate(-1))
+
+poly = Div(Int(50), Mul(Int(5), Div(Int(2), Int(1))))
+print(poly)
+print(poly.evaluate(-1))
+
+poly =  Mul(Mul(Int(5), X()), Int(2))
+print(poly)
+print(poly.evaluate(-1))
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
+print(poly.evaluate(2))
 
 poly = Sub( Sub( Int(4), Int(3)), Sub( X(), Mul( Int(1), Sub( Mul(X(), X()), Int(1)))))
 print(poly)
+print(poly.evaluate(2))
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Div( Int(1), Add( Div(X(), X()), Int(1)))))
 print(poly)
-
-poly = Sub( Sub( Int(4), Int(3)), Sub( X(), Div( Int(1), Sub( Div(X(), X()), Int(1)))))
-print(poly)
-
-poly = Add( Sub( Int(4), Int(3)), Add( X(), Mul( Int(1), Sub( Div(X(), X()), Int(1)))))
-print(poly)
+print(poly.evaluate(2))
 
 poly = Sub( Add( Int(4), Int(3)), Sub( X(), Div( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
+print(poly.evaluate(2))
+
+poly = Add( Sub( Int(4), Int(3)), Add( X(), Mul( Int(1), Sub( Div(X(), X()), Int(1)))))
+print(poly)
+print(poly.evaluate(2))
+
+poly = Sub( Sub( Int(4), Int(3)), Sub( X(), Div( Int(1), Sub( Div(X(), X()), Int(1)))))
+print(poly)
+print(poly.evaluate(2))
