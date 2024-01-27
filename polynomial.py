a5f417bd3d0b2baba9b00454ed1c7e4ad77f2bac
Ashley Harlow <aeharlow@bu.edu>
2024-01-27 14:18:22 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -19,6 +19,14 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        return repr(self.p1) + " - " + repr(self.p2)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -26,14 +34,43 @@ class Mul:
         self.p2 = p2
     
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
+        if isinstance(self.p1, Add) or isinstance(self.p1, Sub):
+            if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
                  return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
             return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
+        if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
 
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        if isinstance(self.p1, Add) or isinstance(self.p1, Sub):
+            if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
+                 return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
+            return "( " + repr(self.p1) + " ) / " + repr(self.p2)
+        if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
+            return repr(self.p1) + " / ( " + repr(self.p2) + " )"
+        return repr(self.p1) + " / " + repr(self.p2)
+
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+print(poly)
+
+poly = Sub( Sub( Int(4), Int(3)), Sub( X(), Mul( Int(1), Sub( Mul(X(), X()), Int(1)))))
+print(poly)
+
+poly = Add( Add( Int(4), Int(3)), Add( X(), Div( Int(1), Add( Div(X(), X()), Int(1)))))
+print(poly)
+
+poly = Sub( Sub( Int(4), Int(3)), Sub( X(), Div( Int(1), Sub( Div(X(), X()), Int(1)))))
+print(poly)
+
+poly = Add( Sub( Int(4), Int(3)), Add( X(), Mul( Int(1), Sub( Div(X(), X()), Int(1)))))
+print(poly)
+
+poly = Sub( Add( Int(4), Int(3)), Sub( X(), Div( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
